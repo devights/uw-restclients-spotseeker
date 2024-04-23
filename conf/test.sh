b24a88fddfe34a96c179c70bc71b34358621847f
David-Strupinski <dstrupin@uw.edu>
2024-04-23 14:02:45 -0700

    try something simpler

--- conf/test.sh
+++ conf/test.sh
@@ -12,7 +12,7 @@ trap catch ERR
 source bin/activate
 
 # install test tooling
-pip install coverage coveralls==3.3.1
+pip install coverage
 
 function run_test {
     echo "##########################"
@@ -25,11 +25,9 @@ function catch {
     exit 1
 }
 
-run_test "coverage run --source=${DJANGO_APP} --omit=*/migrations/*,${DJANGO_APP}/management/commands/* manage.py test ${DJANGO_APP} --settings=project.settings"
+run_test "coverage run --source=${DJANGO_APP} --omit=*/migrations/* manage.py test ${DJANGO_APP} --settings=project.settings"
 
 # put generaged coverage result where it will get processed
 cp .coverage* /coverage
-
-coveralls
 
 exit 0
